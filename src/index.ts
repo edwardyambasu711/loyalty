7ea3459474e457494b47bedf9eb3efe1da6c9abc
edwardyambasu711 <edwardyambasu711@gmail.com>
2026-02-24 11:32:05 +0000

    More

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,9 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: "https://loyalty-hr.vercel.app"
+}));
 app.use(express.json());
 
 // Health check
